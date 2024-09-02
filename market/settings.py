31155e6b56795836f22f04d602f43a9656f7fb7c
N1khilM <nikhilkingh008@gmail.com>
2024-09-02 16:25:55 +0530

    changes req

--- market/settings.py
+++ market/settings.py
@@ -173,7 +173,8 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+#  static root
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # admin
